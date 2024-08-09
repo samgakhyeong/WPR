e621d390a5db1bc072718fc70168bd0f650b2f81
samgak <22789236+samgakhyeong@users.noreply.github.com>
2024-08-09 05:45:08 +0000

    리팩터링

--- assets/item.js
+++ assets/item.js
@@ -3,67 +3,84 @@ window.addEventListener("DOMContentLoaded", (e) => {
 
     let rootElement = document.querySelector("#main .content");
 
-    let row1Element = createDivElement(["row"]);
-    let data1Element = createConetntDivElement(["col", "text-center"], json.title);
-    row1Element.appendChild(data1Element);
+    let row1Element = document.createElement("div");
+    row1Element.classList.add(...["row"]);
 
-    let row2Element = createDivElement(["row"]);
-    let data2Element = createConetntDivElement(["col", "text-center"], json.cooking_method);
-    row2Element.appendChild(data2Element);
-
-    let row3Element = createDivElement(["row"]);
-    let col3Element = createDivElement(["col", "text-center", "d-flex", "justify-content-center"]);
-    let elementTable = document.createElement("table");
-    elementTable.classList.add(...["table", "table-striped", "border", "border-dark"]);
     {
-        let elementThead = document.createElement("thead");
-        let elementTr = document.createElement("tr");
-        let elementTh1 = document.createElement("th");
-        let elementTh2 = document.createElement("th");
-        elementTh1.appendChild(document.createTextNode("이름"));
-        elementTh2.appendChild(document.createTextNode("용량"));
+        let dataElement = document.createElement("div");
+        dataElement.classList.add(...["col", "text-center"]);
+        dataElement.innerText = json.title;
 
-        elementTr.appendChild(elementTh1);
-        elementTr.appendChild(elementTh2);
-        elementThead.appendChild(elementTr);
-        elementTable.appendChild(elementThead);
+        row1Element.appendChild(dataElement);
     }
 
+    let row2Element = document.createElement("div");
+    row1Element.classList.add(...["row"]);
+
     {
-        let elementTbody = document.createElement("tbody");
-        elementTable.appendChild(elementTbody);
-        json.materials.forEach(e => {
+        let dataElement = document.createElement("div");
+        dataElement.classList.add(...["col", "text-center"]);
+        dataElement.innerText = json.cooking_method;
+        row2Element.appendChild(dataElement);
+    }
+
+    let row3Element = document.createElement("div");
+    row3Element.classList.add(...["row"]);
+
+    {
+        let colElement = document.createElement("div");
+        colElement.classList.add(...["col", "text-center", "d-flex", "justify-content-center"]);
+
+        let elementTable = document.createElement("table");
+        elementTable.classList.add(...["table", "table-striped", "border", "border-dark"]);
+        {
+            let elementThead = document.createElement("thead");
             let elementTr = document.createElement("tr");
-            let elementTd1 = document.createElement("td");
-            let elementTd2 = document.createElement("td");
-            let key = Object.keys(e)[0];
-            let values = Object.values(e)[0];
-            elementTd1.innerText = key;
-            elementTd2.innerText = values;
 
-            elementTr.appendChild(elementTd1);
-            elementTr.appendChild(elementTd2);
+            {
+                let elementTh = document.createElement("th");
+                elementTh.appendChild(document.createTextNode("이름"));
+                elementTr.appendChild(elementTh);
+            }
 
-            elementTbody.appendChild(elementTr);
-        });
+            {
+                let elementTh = document.createElement("th");
+                elementTh.appendChild(document.createTextNode("용량"));
+                elementTr.appendChild(elementTh);
+            }
+
+            elementThead.appendChild(elementTr);
+            elementTable.appendChild(elementThead);
+        }
+
+        {
+            let elementTbody = document.createElement("tbody");
+            elementTable.appendChild(elementTbody);
+            json.materials.forEach(e => {
+                let elementTr = document.createElement("tr");
+
+                {
+                    let elementTd = document.createElement("td");
+                    let key = Object.keys(e)[0];
+                    elementTd.innerText = key;
+                    elementTr.appendChild(elementTd);
+                }
+
+                {
+                    let elementTd = document.createElement("td");
+                    let values = Object.values(e)[0];
+                    elementTd.innerText = values;
+                    elementTr.appendChild(elementTd);
+                }
+
+                elementTbody.appendChild(elementTr);
+            });
+        }
+        colElement.appendChild(elementTable);
+        row3Element.appendChild(colElement);
     }
-    col3Element.appendChild(elementTable);
-    row3Element.appendChild(col3Element);
 
     rootElement.appendChild(row1Element);
     rootElement.appendChild(row2Element);
     rootElement.appendChild(row3Element);
 });
-
-function createDivElement(clsssName) {
-    let element = document.createElement("div");
-    element.classList.add(...clsssName);
-    return element
-}
-
-function createConetntDivElement(clsssName, content) {
-    let element = document.createElement("div");
-    element.classList.add(...clsssName);
-    element.innerText = content;
-    return element
-}
